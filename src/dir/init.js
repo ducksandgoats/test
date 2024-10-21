eefa80d990af61392489c9ac40587403a0750eca
ducksandgoats <mrtraderjoseph@gmail.com>
2024-10-20 22:40:32 -0500

    make changes

--- src/dir/init.js
+++ src/dir/init.js
@@ -1,7 +1,7 @@
 import dexieFunc from './dexie.js'
 import gunFunc from './gun.js'
 
-const base = dexieFunc({debug: true, version: 1, url: 'ws://198.46.188.206:10509/signal', hash: '7e6520e2fe505702ec644226ccb0a8bc467c5a2c', name: 'testing', schema: {test: 'id, stamp, edit, text'}})
+const base = dexieFunc({debug: true, version: 1, url: 'ws://198.46.188.206:10509/signal', hash: '7e6520e2fe505702ec644226ccb0a8bc467c5a2c', name: 'test', schema: {test: 'id, connection, try, stamp, edit,'}})
 const gun = gunFunc({url: 'ws://107.173.15.203:10509/signal', hash: '7e6520e2fe505702ec644226ccb0a8bc467c5a2c', debug: true})
 const msg = (async () => {return (await fetch('msg://test')).body})()
 const pubsub = (async () => {return (await fetch('pubsub://test')).body})()
