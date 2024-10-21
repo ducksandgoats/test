1ff1ae14346f287372d1d9cdf6333aac77542e35
ducksandgoats <mrtraderjoseph@gmail.com>
2024-10-20 21:59:56 -0500

    make changes

--- src/App.svelte
+++ src/App.svelte
@@ -40,4 +40,13 @@
     will-change: filter;
     transition: filter 300ms;
   }
+  .logo:hover {
+    filter: drop-shadow(0 0 2em #646cffaa);
+  }
+  .logo.svelte:hover {
+    filter: drop-shadow(0 0 2em #ff3e00aa);
+  }
+  .read-the-docs {
+    color: #888;
+  }
 </style>
