5f34f2d9c39431b186e5230db1a394249ab6c222
ducksandgoats <mrtraderjoseph@gmail.com>
2024-10-20 22:40:06 -0500

    make changes

--- src/dir/init.js
+++ src/dir/init.js
@@ -5,6 +5,6 @@ const base = dexieFunc({debug: true, version: 1, url: 'ws://198.46.188.206:10509
 const gun = gunFunc({url: 'ws://107.173.15.203:10509/signal', hash: '7e6520e2fe505702ec644226ccb0a8bc467c5a2c', debug: true})
 const msg = (async () => {return (await fetch('msg://test')).body})()
 const pubsub = (async () => {return (await fetch('pubsub://test')).body})()
-const topic = (async () => {return (await fetch('topic://test')).body})()
+const topic = (async () => {return (await fetch('topic://test')).body})(
 
 export {base, gun, msg, pubsub, topic}
