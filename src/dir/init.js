510e8a093f490fe824d4316ce90de75b2e58a50f
ducksandgoats <mrtraderjoseph@gmail.com>
2024-10-27 02:28:48 -0500

    make changes

--- src/dir/init.js
+++ src/dir/init.js
@@ -1,8 +1,8 @@
 import dexieFunc from 'dexie-relay'
 import gunFunc from 'gun-relay'
 
-const base = dexieFunc({debug: true, version: 1, url: 'ws://198.46.188.206:10509/signal', hash: '7e6520e2fe505702ec644226ccb0a8bc467c5a2c', name: 'testing', schema: {test: 'id, stamp, edit, text'}})
-const gun = gunFunc({url: 'ws://107.173.15.203:10509/signal', hash: '7e6520e2fe505702ec644226ccb0a8bc467c5a2c', debug: true})
+const base = dexieFunc({debug: true, version: 1, url: '198.46.188.206:10509', hash: '7e6520e2fe505702ec644226ccb0a8bc467c5a2c', name: 'testing', schema: {test: 'id, stamp, edit, text'}})
+const gun = gunFunc({url: '107.173.15.203:10509', hash: '7e6520e2fe505702ec644226ccb0a8bc467c5a2c', debug: true})
 const msg = (async () => {return (await fetch('msg://test')).body})()
 const pubsub = (async () => {return (await fetch('pubsub://test')).body})()
 const topic = (async () => {return (await fetch('topic://test')).body})()
