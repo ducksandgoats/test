323ef0f7331a6e2f31e6ceb76b332f584ba4d532
ducksandgoats <mrtraderjoseph@gmail.com>
2024-10-30 22:26:42 -0500

    make changes

--- src/dir/init.js
+++ src/dir/init.js
@@ -2,6 +2,6 @@ import Base from 'dexie-relay'
 import gunFunc from 'gun-relay'
 
 const base = new Base({debug: true, version: 1, url: '198.46.188.206:10509', hash: '7e6520e2fe505702ec644226ccb0a8bc467c5a2c', name: 'test', schema: {test: 'id, stamp, edit, text'}, sync: true})
-const gun = gunFunc({url: '107.173.15.203:10509', hash: '7e6520e2fe505702ec644226ccb0a8bc467c5a2c', debug: true})
+const gun = new gunFunc({url: '107.173.15.203:10509', hash: '7e6520e2fe505702ec644226ccb0a8bc467c5a2c', debug: true})
 
 export {base, gun}
